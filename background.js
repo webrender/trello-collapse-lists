d71ee00e9f0cdb748ecae00e34aff2491aa6b87b
Jeremy Laskar <webrender@gmail.com>
2017-07-31 10:15:57 -0700

    bugfix

--- background.js
+++ background.js
@@ -1,16 +1,20 @@
-var injectTimeout;
+var injectTimeout, currentUrl;
 
 function inject() {
     chrome.tabs.executeScript(null,{file:"script.js"});
 }
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    console.log(details);
     if(details.url.indexOf('https://trello.com/b/') > -1) {
-        if (!injectTimeout) {
-            injectTimeout = setTimeout(inject, 1000);
-        } else {
-            clearTimeout(injectTimeout);
-            injectTimeout = setTimeout(inject, 1000);
+        if (details.url.substr(0,30) != currentUrl) {
+            currentUrl = details.url.substr(0,30);
+            if (!injectTimeout) {
+                injectTimeout = setTimeout(inject, 1000);
+            } else {
+                clearTimeout(injectTimeout);
+                injectTimeout = setTimeout(inject, 1000);
+            }
         }
     }
 });
