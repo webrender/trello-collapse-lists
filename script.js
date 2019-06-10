4a127fe3e8558f0c2badc98b6a87f70cb0eda881
Jeremy Laskar <jeremy.laskar@fictiv.com>
2019-06-10 11:48:31 -1000

    1.1.7: refactor drag logic

--- script.js
+++ script.js
@@ -1,67 +1,82 @@
-if (!document.querySelector('.collapse-toggle')) {
-    // get boardid
-    var boardid = window.location.href.substring(window.location.href.indexOf('/b/') + 3, window.location.href.indexOf('/b/') + 11);
-    // get all lists
-    document.querySelectorAll('.list-header-name').forEach( e => {
-        // encoded list title for unique id
-        var columnName = encodeURI(e.textContent);
-        // get isClosed value from chrome extension storage
-        chrome.storage.local.get(boardid+':'+columnName, isClosed => {
-            // if this list is closed, add the -closed class
-            if (isClosed[boardid+':'+columnName])
-                e.parentNode.parentNode.parentNode.classList.add('-closed');
-            // create toggle button
-            var toggle = document.createElement("div");
-            toggle.className = 'collapse-toggle';
-            // toggle click handler
-            toggle.addEventListener('click', evt => {
-                // get column name from event target
-                var thisColumn = encodeURI(evt.target.nextSibling.textContent);
-                // set isClosed value in chrome storage to inverse value
-                chrome.storage.local.set({[boardid+':'+thisColumn]: isClosed[boardid+':'+columnName] ? null : true}, res => {
-                    // toggle the -closed class on successful save
-                    evt.target.parentNode.parentNode.parentNode.classList.toggle('-closed');
-                });
-            })
-            e.parentNode.parentNode.parentNode.setAttribute('draggable', true);
-            // insert toggle button
-            e.parentNode.insertBefore(toggle, e);
-        });
+if (!document.querySelector(".collapse-toggle")) {
+  // get boardid
+  var boardid = window.location.href.substring(
+    window.location.href.indexOf("/b/") + 3,
+    window.location.href.indexOf("/b/") + 11
+  );
+  // get all lists
+  document.querySelectorAll(".list-header-name").forEach(e => {
+    // encoded list title for unique id
+    var columnName = encodeURI(e.textContent);
+    // get isClosed value from chrome extension storage
+    chrome.storage.local.get(boardid + ":" + columnName, isClosed => {
+      // if this list is closed, add the -closed class
+      if (isClosed[boardid + ":" + columnName]) {
+        e.parentNode.parentNode.parentNode.classList.add("-closed");
+        e.parentNode.parentNode.parentNode.classList.add("-cl");
+      }
+      // create toggle button
+      var toggle = document.createElement("div");
+      toggle.className = "collapse-toggle";
+      // toggle click handler
+      toggle.addEventListener("click", evt => {
+        // get column name from event target
+        var thisColumn = encodeURI(evt.target.nextSibling.textContent);
+        // set isClosed value in chrome storage to inverse value
+        chrome.storage.local.set(
+          {
+            [boardid + ":" + thisColumn]: isClosed[boardid + ":" + columnName]
+              ? null
+              : true
+          },
+          res => {
+            // toggle the -closed class on successful save
+            evt.target.parentNode.parentNode.parentNode.classList.toggle(
+              "-closed"
+            );
+          }
+        );
+      });
+      //   e.parentNode.parentNode.parentNode.setAttribute("draggable", true);
+      // insert toggle button
+      e.parentNode.insertBefore(toggle, e);
     });
-    // we want to open lists after a short delay if a user is dragging a card on top of one
-    // trello already uses jQuery draggable, but we have to create new events since content scripts
-    // cant access JS on the parent page.
-    var isClosed, openList;
-    // make all cards draggable - revert to their former location if they werent moved, and don't wait to revert after drop
-    $('.list-card').draggable({revert: true, revertDuration: 0});
-    // make all lists droppable
-    $('.js-list').droppable({
-        // we only want to look at the area below the pointer
-        tolerance: 'pointer',
-        // when we move over a column, if it's closed, open it after a short period.
-        // we use isClosed to keep track of the list's initial state so we know if we need to close it after.
-        over: (evt, ui) => {
-            if (evt.target.classList.contains('-closed')) {
-                openList = setTimeout(() => {
-                    evt.target.classList.remove('-closed');
-                    isClosed = true;
-                }, 250);
-            } else {
-                isClosed = false;
-            }
-        },
-        // when we leave a list or drop an item on it, clear the timeout and close it if it was originally closed.
-        out: (evt, ui) => {
-            clearTimeout(openList);
-            if (isClosed) {
-                evt.target.classList.add('-closed');
-            }
-        },
-        drop: (evt, ui) => {
-            clearTimeout(openList);
-            if (isClosed) {
-                evt.target.classList.add('-closed');
-            }
+  });
+  // we want to open lists after a short delay if a user is dragging a card on top of one
+  // trello already uses jQuery draggable, but we have to create new events since content scripts
+  // cant access JS on the parent page.
+  var isClosed, openList;
+  $(".list-card").draggable({
+    helper: "clone",
+    // we used to use a jquery droppable event to monitor drop events, but either trello or jquery ui
+    // changed something that broke this.  now, while a user is dragging, we use the code below to 
+    // evaluate if the pointer is over a closed list and react accordingly
+    drag: e => {
+      document.querySelectorAll(".js-list.-cl").forEach(l => {
+        const c = l.getBoundingClientRect();
+        if (
+          e.pageX > c.left &&
+          e.pageX < c.right &&
+          e.pageY > c.top &&
+          e.pageY < c.bottom
+        ) {
+          if (!openList) {
+            // we use a timeout here so that users can drag cards across a closed list without
+            // opening it right away.
+            openList = setTimeout(() => {
+              l.classList.add("-open");
+              l.classList.remove("-closed");
+            }, 250)
+          }
+        } else {
+          clearTimeout(openList)
+          openList = null
+          if (l.classList.contains("-open")) {
+            l.classList.remove("-open");
+            l.classList.add("-closed");
+          }
         }
-    });
+      });
+    }
+  });
 }
